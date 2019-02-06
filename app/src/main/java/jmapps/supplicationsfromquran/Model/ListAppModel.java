60e4593abfd4222af77961c45b07b60904afe5ad
JMApps <jmappsmuslim@gmail.com>
2019-02-06 22:39:33 +0300

    Replaced icons

--- app/src/main/java/jmapps/supplicationsfromquran/Model/ListAppModel.java
+++ app/src/main/java/jmapps/supplicationsfromquran/Model/ListAppModel.java
@@ -9,23 +9,23 @@ public class ListAppModel {
     private final String strLinkApp;
 
     public static final ListAppModel[] listAppModel = {
-            new ListAppModel(R.drawable.notes, "Заметки",
+            new ListAppModel(R.drawable.notes_small, "Заметки",
                     "https://play.google.com/store/apps/details?id=jmapps.simplenotes"),
-            new ListAppModel(R.drawable.supplications, "Мольбы из Корана",
+            new ListAppModel(R.drawable.supplications_small, "Мольбы из Корана",
                     "https://play.google.com/store/apps/details?id=jmapps.supplicationsfromquran"),
-            new ListAppModel(R.drawable.arabic, "Арабский в твоих руках. Том 1",
+            new ListAppModel(R.drawable.arabicinyourhands_small, "Арабский в твоих руках. Том 1",
                     "https://play.google.com/store/apps/details?id=jmapps.arabicinyourhands"),
-            new ListAppModel(R.drawable.lessons, "Уроки Рамадана",
+            new ListAppModel(R.drawable.lessons_small, "Уроки Рамадана",
                     "https://play.google.com/store/apps/details?id=jmapps.lessonsoframadan"),
-            new ListAppModel(R.drawable.fortress, "Крепость мусульманина",
+            new ListAppModel(R.drawable.fortress_small, "Крепость мусульманина",
                     "https://play.google.com/store/apps/details?id=jmapps.fortressofthemuslim"),
-            new ListAppModel(R.drawable.thenames, "Толкование прекрасных имён Аллаха",
+            new ListAppModel(R.drawable.thenames_small, "Толкование прекрасных имён Аллаха",
                     "https://play.google.com/store/apps/details?id=jmapps.thenamesof"),
-            new ListAppModel(R.drawable.strength, "Сила воли",
+            new ListAppModel(R.drawable.strength_small, "Сила воли",
                     "https://play.google.com/store/apps/details?id=jmapps.strengthofwill"),
-            new ListAppModel(R.drawable.questions, "200 вопросов по вероучению Ислама",
+            new ListAppModel(R.drawable.questions_small, "200 вопросов по вероучению Ислама",
                     "https://play.google.com/store/apps/details?id=jmapps.questions200"),
-            new ListAppModel(R.drawable.hadith, "40 хадисов имама ан-Навави",
+            new ListAppModel(R.drawable.hadith_small, "40 хадисов имама ан-Навави",
                     "https://play.google.com/store/apps/details?id=jmapps.hadith40"),
 
     };
